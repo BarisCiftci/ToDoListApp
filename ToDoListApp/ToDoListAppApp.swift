8b75e8598b4a73785cf213089f473137c538136b
Baris Ciftci <barishciftci@gmail.com>
2023-02-09 22:09:16 +0100

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Baris Ciftci on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
